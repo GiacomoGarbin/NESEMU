5d35d3fe400d7f7e7fa0611bdb4989f02b6f423e
Giacomo Garbin <giacomogarbinmailbox@gmail.com>
2025-04-06 18:49:52 +0200

    Connect CPU and RAM to the BUS and define Read/Write BUS methods.

--- Bus.cpp
+++ Bus.cpp
@@ -1,1 +1,34 @@
 #include "Bus.h"
+
+Bus::Bus()
+{
+    // clear RAM
+
+    for (uint8_t& byte : mRAM)
+    {
+        byte = 0x00;
+    }
+}
+
+Bus::~Bus()
+{
+
+}
+
+void Bus::Write(uint16_t addr, uint8_t data)
+{
+    if ((0x0000 <= addr) && (addr <= 0xFFFF))
+    {
+        mRAM[addr] = data;
+    }
+}
+
+uint8_t Bus::Read(uint16_t addr, bool bReadOnly = false)
+{
+    if ((0x0000 <= addr) && (addr <= 0xFFFF))
+    {
+        return mRAM[addr];
+    }
+
+    return 0x00;
+}
--- Bus.h
+++ Bus.h
@@ -1,8 +1,22 @@
 #pragma once
 
+#include "CPU.h"
+
+#include <array>
+#include <cstdint>
+
 class Bus
 {
 public:
     Bus();
     ~Bus();
+
+    using RAM = std::array<uint8_t, 64*1024>;
+
+    // devices connected to the bus
+    CPU mCPU;
+    RAM mRAM;
+
+    void Write(uint16_t addr, uint8_t data);
+    uint8_t Read(uint16_t addr, bool bReadOnly = false);
 };
